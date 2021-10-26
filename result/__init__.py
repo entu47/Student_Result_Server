08336995a1658383078e9985b7f595f1cb05623e
akhilesh <ak475885@gmail.com>
2021-10-26 13:56:55 +0530

    Data Base set for heroku

--- result/__init__.py
+++ result/__init__.py
@@ -9,7 +9,7 @@ from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:test@localhost/empfynd'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('JAWSDB_URL')
 db = SQLAlchemy(app)
 # bcrypt = Bcrypt(app)
 # login_manager = LoginManager(app)
